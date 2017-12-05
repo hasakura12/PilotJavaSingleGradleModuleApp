288bd10ecf801f278fbb112c83e7e64a659a2371
Asakura, Hisashi <Hisashi.Asakura@ca.com>
2017-12-05 13:38:40 -0800

    Added external library and made the project runnable using ./gradlew run

--- src/main/java/core/HelloWorld.java
+++ src/main/java/core/HelloWorld.java
@@ -1,17 +1,22 @@
 package main.java.core;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import org.joda.time.LocalTime;
 
 /**
  * Created by asahi02 on 2017-12-05.
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
-        System.out.println("The current local time is: " + dtf.format(now));
+        // this is native library
+//        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+//        LocalDateTime now = LocalDateTime.now();
+//        System.out.println("The current local time is: " + dtf.format(now));
+
+        // this is using external library
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
